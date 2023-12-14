089526244e284efdb5cd4707dcd32cf515f8ed38
Teerasak Jumwanta <vgthailand@hotmail.com>
2023-12-14 18:00:43 +0700

    Update 4

---
+++ MRT_Demo/Models/PredicOwnerMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MRT_Demo.Models
+{
+    public class PredicOwnerMetadata
+    {
+    }
+
+    [MetadataType(typeof(PredicOwnerMetadata))]
+    public partial class PredicOwner
+    {
+        public bool IsDeletePredicOwner { get; set; }
+    }
+}
